96f234043ec7eb44ce9aa030ae4e3735187651c4
inian <inian1234@gmail.com>
2012-10-25 05:32:20 +0800

    basic logged in done

--- conjunct/app/models/user.rb
+++ conjunct/app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   attr_accessible :name, :nric, :phone, :email, :password
 
   validates :password, :presence => true
+
+ def my_password
+   @password ||= Password.new(password)
+ end
+
+  def self.authenticate(username, password)
+    user = User.find_by_name(username)
+    if !user.nil?
+      if user.my_password == password
+        user
+      else
+        nil
+      end
+    end
+  end
 end
