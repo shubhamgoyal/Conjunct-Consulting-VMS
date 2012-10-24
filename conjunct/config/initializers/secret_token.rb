bc54bacec590e432810914b8eebe76231a60bd59
inian <inian1234@gmail.com>
2012-10-25 01:27:20 +0800

    basic rails application added

---
+++ conjunct/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conjunct::Application.config.secret_token = '95f6937fd023892461901a6641268e6f3cfc499e936cb6e27efc367a495d832a8f2fb7d75f4b0da2a061a14a54ae5229de01e32ce1b518ab8190b8780e0d340b'
